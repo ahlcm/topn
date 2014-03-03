37003111e8d90d85dfa7c8ca00456b8bafc955d2
lichunming <lichunming@lightinthebox.com>
2014-03-03 17:51:49 +0800

    test
    test a upload.php

---
+++ upload.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<title>Deal/Coupon of </title>
+	<meta name="description" content="Deal/Coupon of ">
+	<meta name="keywords" content="Coupon, Deal, ">
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+	<link rel="stylesheet" href="css/site.css" type="text/css" media="all">
+	<link rel="stylesheet" href="css/style-client.css" type="text/css" media="all">
+	<link rel="stylesheet" href="css/jquery-ui-lcm.css" type="text/css" media="all">
+
+	<link rel="StyleSheet" href="css/sidebar.css" type="text/css">
+	<link rel="StyleSheet" href="css/dialog.css" type="text/css">
+	<script type="text/javascript" src="/js/site.js"></script>
+	<script type="text/javascript" src="http://www.ahlcm.com:8081/js/maxheight.js"></script>
+	<script type="text/javascript" src="http://www.ahlcm.com:8081/js/jquery-1.9.1.js"></script>
+	<script type="text/javascript" src="http://www.ahlcm.com:8081/js/jquery.faded.js"></script>
+	<script type="text/javascript" src="http://www.ahlcm.com:8081/js/jquery.jqtransform.js"></script>
+	<script type="text/javascript" src="http://www.ahlcm.com:8081/js/jquery-ui-lcm.js"></script>
+	<script type="text/javascript" src="js/deals.js?rev=26389"></script>
+	<script type="text/javascript" src="/js/dealeditor.js"></script>
+	<!--[if lt IE 7]>
+	<script type="text/javascript" src="http://info.template-help.com/files/ie6_warning/ie6_script_other.js"></script>
+	<![endif]-->
+	<!--[if lt IE 9]>
+	<script type="text/javascript" src="js/html5.js"></script>
+	<![endif]-->
+	<link href="http://www.ahlcm.com:8081/js/he/themes/default/css/ueditor.css" type="text/css" rel="stylesheet"><script src="http://www.ahlcm.com:8081/js/he/third-party/codemirror/codemirror.js" type="text/javascript" defer="defer"></script><link rel="stylesheet" type="text/css" href="http://www.ahlcm.com:8081/js/he/third-party/codemirror/codemirror.css">
+</head><body>
+<?php
+require_once 'bootstrap.php';
+
+require 'client/templates/open-deal-dialog.phtml';
+?>
+</body>
+</html>
